a18d982749e86626a63abe6eb6b9f2e844bfcb93
ismael4174 <ouattara4174@gmail.com>
2024-11-14 16:19:39 +0000

    Nouvel exercice

--- src/ci/model/Affichage.java
+++
@@ -1,6 +0,0 @@
-package ci.model;
-
-public interface Affichage {
-
-    public void afficherv2();
-}
--- src/ci/model/Etudiant.java
+++
@@ -1,9 +0,0 @@
-package ci.model;
-
-public class Etudiant extends Personne {
-
-    public Etudiant(){
-        super("nom1", "prenom1");
-    }
-    
-}
