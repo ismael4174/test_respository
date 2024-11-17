777c84f32a1cc59fc7c94facd07cea3f4a4be870
ismael4174 <ouattara4174@gmail.com>
2024-11-17 16:12:34 +0000

    Enquete et App

--- src/App.java
+++ src/App.java
@@ -1,8 +1,28 @@
+import ci.models.TypeEnquete;
+import ci.models.Enquete;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Hello, World!");
-        
+
+        // System.out.println("Hello, World!");
+
+        TypeEnquete t1 = new TypeEnquete(1, "Info", "Informatique");
+        TypeEnquete t2 = new TypeEnquete(2, "Edu", "Education");
+
+        t1.afficher();
+        t2.afficher();
+        System.out.println("------------------------------------------------------------");
+        Enquete e1 = new Enquete(10, "Enquête sur le matériel informatique", "Analyse sur l etat du matériel", t1);
+        Enquete e2 = new Enquete(11, "Enquête le niveau d éducation", "Étude du QI", t2);
+
+        e1.afficherv2();
+        e2.afficherv2();
+        System.out.println("------------------------------------------------------------");
+
+        Enquete[] enquetes = { e1, e2 };
+        for (Enquete enquete : enquetes) {
+            enquete.afficherv3();
+        }
+
     }
 }
