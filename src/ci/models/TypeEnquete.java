887bdfa742d95ef2aefe71dcc87feb58719e5ac1
ismael4174 <ouattara4174@gmail.com>
2024-11-17 15:51:08 +0000

    Création de la class TypeEnquete

---
+++ src/ci/models/TypeEnquete.java
@@ -0,0 +1,45 @@
+package ci.models;
+
+public class TypeEnquete {
+    private int id;
+    private String code;
+    private String libelle;
+
+    public TypeEnquete(int id, String code, String libelle) {
+        this.id = id;
+        this.code = code;
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+   
+    public void afficher() {
+        System.out.println(
+                        "Id : " + this.id + " Code : " + this.code + " Libelle : " + this.libelle
+                                        );
+}
+
+
+}
